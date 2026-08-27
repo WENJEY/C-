09698c8b8642e15aad70570304328e67aeac681f
WENJEY <yunw.j07@gmail.com>
2026-08-28 02:09:18 +0800

    Add files via upload

---
+++ staff_internal.h
@@ -0,0 +1,30 @@
+#ifndef STAFF_INTERNAL_H
+#define STAFF_INTERNAL_H
+#include "hotel.h"
+
+void staffRoomMenu();
+void staffLocationMenu();
+void staffReports();
+void staffViewAllBookings();
+void staffSearchCustomer();
+void staffTodayCheckIns();
+
+bool roomHasActiveBooking(const string& roomNumber);
+bool askLine(const string& prompt, string& out);
+bool askMoney(const string& prompt, double& amount);
+string pickStateForStaff(bool allowNew);
+string pickAreaForStaff(const string& state, bool allowNew);
+int pickHotelIndexForStaff(const string& state, const string& area);
+string pickRoomStatus();
+string pickRoomType();
+void showStaffHotel(int idx);
+void staffPrintRooms();
+void staffListRooms();
+void staffAddRoom();
+void staffEditRoom();
+void staffDeleteRoom();
+void staffListHotels();
+void staffAddHotel();
+void staffEditHotel();
+void staffDeleteHotel();
+#endif
